8777f9116422736fe3f52c2327c28e9280c6bcb5
Nathan Stitt <nathan@stitt.org>
2025-02-18 14:42:13 -0600

    editor spacing

--- testing/index.ts
+++ testing/index.ts
@@ -6,7 +6,5 @@ export const localPath = (url: string) => path.dirname(fileURLToPath(url))
 
 export const supportingFilesDirname = localPath(import.meta.url)
 
-export const readPrivateKey = () =>
-  fs.readFileSync(`${supportingFilesDirname}/private_key.pem`, 'utf8')
-export const readPublicKey = () =>
-  fs.readFileSync(`${supportingFilesDirname}/public_key.pem`, 'utf8')
+export const readPrivateKey = () => fs.readFileSync(`${supportingFilesDirname}/private_key.pem`, 'utf8')
+export const readPublicKey = () => fs.readFileSync(`${supportingFilesDirname}/public_key.pem`, 'utf8')
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    mockReset: true,
-    environment: 'happy-dom',
-    include: ['**/*.(test).{js,jsx,ts,tsx}'],
-  },
+    test: {
+        mockReset: true,
+        environment: 'happy-dom',
+        include: ['**/*.(test).{js,jsx,ts,tsx}'],
+    },
 })
