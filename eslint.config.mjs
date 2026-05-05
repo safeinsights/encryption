68c1af3166ab8527cc2fe6efa32026a7bb05b1c4
Scott Batchelder <battibatch@gmail.com>
2026-05-05 16:52:01 -0500

    migrating bidi to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import antiTrojanSource from 'eslint-plugin-anti-trojan-source'
 
 export default [
     { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -7,6 +8,10 @@ export default [
 
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
+    {
+        plugins: { 'anti-trojan-source': antiTrojanSource },
+        rules: { 'anti-trojan-source/no-bidi': 'error' },
+    },
     {
         rules: {
             'no-console': ['error', { allow: ['warn', 'error'] }],
